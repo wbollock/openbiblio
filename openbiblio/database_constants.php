6086426b71b891cd72c930407912471a5c13e25b
Will Bollock <wbollock@gmail.com>
2019-06-12 09:20:10 -0400

    init

---
+++ openbiblio/database_constants.php
@@ -0,0 +1,19 @@
+<?php
+/*********************************************************************************
+ *
+ *                           A T T E N T I O N !
+ *
+ *  ||  Please modify the following database connection variables to match  ||
+ *  \/  the MySQL database and user that you have created for OpenBiblio.   \/
+ *********************************************************************************
+ */
+define("OBIB_HOST",     "localhost");
+define("OBIB_DATABASE", "openbiblio");
+define("OBIB_USERNAME", "openbiblio");
+define("OBIB_PWD",      "openbiblio");
+/*********************************************************************************
+ *  /\                                                                      /\
+ *  ||                                                                      ||
+ *********************************************************************************
+ */
+?>
